de9e1f1284b6dd12314116142e4658957c5c09c0
kashish <kgupta7827@gmail.com>
2026-05-30 14:14:05 +0530

    feat: add ChatInterface component and update chat page layout

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,26 +1,48 @@
 "use client"
 import { PiBookOpenUserLight } from "react-icons/pi";
+import { TiPlus } from "react-icons/ti";
+import { FaHistory } from "react-icons/fa";
+import ChatInterface from "@/Components/ChatInterface";
+
 const page = () => {
   return (
     <div className='h-full w-full flex justify-center items-center'>
-      <div className='flex w-full'>
-        {/* sidebar */}
-        <div className='w-1/6 border-r h-screen border-white/15 p-5'>
-          <div className="flex justify-between ">
-            <li className="flex items-center gap-1 cursor-pointer hover:opacity-80 transition-opacity">
-              <span>OPEN</span>
-              <span className="text-black bg-white text-sm px-2 py-1 font-bold">PAPERS</span>
-            </li>
+      <div className='flex w-full m-2 mt-3'>
 
-            <PiBookOpenUserLight className="text-3xl"/>
+        <div className='h-screen w-full flex'>
+          {/* sidebar */}
+          <div className='w-1/6 border-r border-white/15 p-2'>
+            <div className="flex justify-between">
+              <li className="flex items-center gap-1 cursor-pointer hover:opacity-80 transition-opacity list-none">
+                <span>OPEN</span>
+                <span className="text-black bg-white text-sm px-2 py-1 font-bold">
+                  PAPERS
+                </span>
+              </li>
+
+              <PiBookOpenUserLight className="text-3xl" />
+            </div>
+
+            <div className="mt-6">
+              <button className="group flex items-center gap-3 w-full py-2 rounded-xl text-gray-300 hover:bg-zinc-800 transition-all duration-200">
+                <TiPlus className="text-lg group-hover:rotate-90 transition-transform duration-200" />
+                <span className="font-medium">New Chat</span>
+              </button>
+            </div>
+            <div className="">
+              <button className="group flex items-center gap-3 w-full py-2 rounded-xl text-gray-300 hover:bg-zinc-800 transition-all duration-200">
+                <FaHistory className="text-lg group-hover:rotate-90 transition-transform duration-200" />
+                <span className="font-medium">History</span>
+              </button>
+            </div>
+          </div>
+
+          {/* chat */}
+          <div className='w-5/6 p-5 flex items-center justify-center'>
+            <ChatInterface/>
           </div>
         </div>
 
-
-        {/* chat */}
-        <div className='w-5/6'>
-          chat
-        </div>
 
       </div>
     </div>
